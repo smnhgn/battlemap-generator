ad594a9f5ce0d9a855bc90584d4fc0619f3b40f8
Simon Haugen <smn.hgn@gmail.com>
2020-04-19 14:13:02 +0200

    added generator component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneratorComponent } from './generator/generator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'generator'
+  },
+  {
+    path: 'generator',
+    component: GeneratorComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'generator'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
